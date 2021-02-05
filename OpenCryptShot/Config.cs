2ca147088450b995d90cc9543ed6d2bdb9a281bd
MasterKush <jonathan.roy282@gmail.com>
2021-02-05 09:36:38 -0500

    Fix bugs with orders and disable LogVerbosity.

--- OpenCryptShot/Config.cs
+++ OpenCryptShot/Config.cs
@@ -6,6 +6,7 @@
         public string apiSecret;
         public decimal quantity; // the quantity of BTC to use
         public decimal takeProfitRate; // the target profit (1.0 is no profit, 2.0 is 100% profit)
-        public decimal stopLossRate; // stop loss rate (0.8 is 20% loss)
+        public decimal limitPriceRate; // trigger stopLoss rate (0.8 is 20% loss)
+        public decimal stopLossRate; // stop loss rate (0.75 is 25% loss)
     }
 }
