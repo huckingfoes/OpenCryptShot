80f20161679a91a9ca449aefbb1beca0a1f3a94c
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-02-19 02:33:14 +0100

    Add Discord scraping

---
+++ OpenCryptShot/Discord/Message.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenCryptShot.Discord
+{
+    public class Message
+    {
+        public string id { get; set; }
+        public int type { get; set; }
+        public string content { get; set; }
+        public string channel_id { get; set; }
+        public bool mention_everyone { get; set; }
+        public string timestamp { get; set; }
+        public string edited_timestamp { get; set; }
+        public int flags { get; set; }
+    }
+}
